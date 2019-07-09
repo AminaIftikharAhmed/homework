7615e0ffafc179e7e4a46467749311da9261fedc
AminaIftikharAhmed <50007605+AminaIftikharAhmed@users.noreply.github.com>
2019-07-09 01:27:46 -0700

    if else today work

---
+++ ifelseStatement/ifelseStatement.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>If else Statement</title>
+</head>
+<body>
+
+	<h2>If else Statement</h2>
+
+
+	<?php
+
+         $t = date("H");
+
+         if ($t>"20") {
+         	
+
+         	echo "It is sunny day!";
+         }
+
+         else{
+
+         	echo "It is cool evening!";
+         }
+
+     ?>
+	
+</body>
+</html>
