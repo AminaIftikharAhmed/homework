621face4473844fffa357d8a66ad931f97d19e93
AminaIftikharAhmed <50007605+AminaIftikharAhmed@users.noreply.github.com>
2019-07-09 22:55:40 -0700

    Foreach loop

---
+++ foreach/foreach.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title> Foreach </title>
+</head>
+<body>
+	
+     <?php 
+
+		$colors = array("red", "green", "blue", "yellow","pink", "skyblue"); 
+
+		foreach ($colors as $value) {
+
+		    echo "$value <br>";
+
+		}
+
+     ?>
+
+</body>
+</html>
