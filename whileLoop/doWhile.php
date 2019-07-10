8f5d646f74e2f1c72a331c8f63a337dc2875a06d
AminaIftikharAhmed <50007605+AminaIftikharAhmed@users.noreply.github.com>
2019-07-09 22:56:49 -0700

    While and do while loop

---
+++ whileLoop/doWhile.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Do While Loop</title>
+</head>
+<body>
+
+	<?php 
+
+	$x = 1; 
+
+	do {
+
+	    echo "The number is: $x <br>";
+
+	    $x++;
+
+	} while ($x <= 10);
+
+
+	/////////////////////////////        Decrement          /////////////////////
+
+	$x = 10; 
+
+	do {
+
+	    echo "The number is: $x <br>";
+
+	    $x--;
+
+	} while ($x >= 0);
+
+
+	?>
+
+
+
+	
+</body>
+</html>
---
+++ whileLoop/while.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>While Loop</title>
+</head>
+<body>
+
+	<?php
+
+      $x = 1;
+
+      while ($x <= 10) {
+
+      	echo "The number is $x  <br>";
+
+      	$x++;   
+
+      }
+
+
+///////////////////////////   Decrement   ////////////////////////////////////////////
+
+       $x = 20;
+
+      while ($x >= 1) {
+
+      	echo "The number is $x  <br>";
+
+      	$x--; 
+
+      }
+
+
+	?>
+	
+</body>
+</html>
