663c6ef465729be3bbf18f389033303bec789743
AminaIftikharAhmed <50007605+AminaIftikharAhmed@users.noreply.github.com>
2019-07-09 22:56:01 -0700

    Forloop

---
+++ forLoop/forLoop.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>For Loop</title>
+</head>
+<body>
+
+	<?php 
+
+		for ($x = 0; $x <= 10; $x++) {
+
+		    echo "The number is: $x <br>";
+
+		} 
+
+     ?>
+	
+</body>
+</html>
