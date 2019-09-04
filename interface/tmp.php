fa1f893bc3b405a209e851eea910813ceae22eb6
AminaIftikharAhmed <50007605+AminaIftikharAhmed@users.noreply.github.com>
2019-09-04 12:03:19 -0700

    Add files via upload

---
+++ interface/tmp.php
@@ -0,0 +1,13 @@
+<?php 
+
+ <?php 
+                component("Product1", 499, "./upload/pro1.png");
+                component("Product2", 599, "./upload/pro2.png");
+                component("Product3", 699, "./upload/pro3.png");
+                component("Product4", 799, "./upload/pro4.png");
+
+ ?>
+
+
+
+ ?>
